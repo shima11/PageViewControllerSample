62cd52dc410e95777925cd46f6ef8d2663b3b30e
shima <shima11@users.noreply.github.com>
2017-01-17 03:08:54 +0900

    PageViewControllerを実装

--- PageViewControllerSample/ViewController.swift
+++ PageViewControllerSample/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let storyboard = UIStoryboard(storyboard: .PageViewController)
+        let pageVC: PageViewController = storyboard.instantiateViewController()
+        
+        //        present(pageVC, animated: false, completion: nil)
+        
+        addChildViewController(pageVC)
+        view.addSubview(pageVC.view)
+        pageVC.didMove(toParentViewController: self)
     }
 
     override func didReceiveMemoryWarning() {
