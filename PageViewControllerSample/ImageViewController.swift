a4148d5bc853893f0be1317c818ee71efa1bb137
shima <shima11@users.noreply.github.com>
2017-01-17 03:07:30 +0900

    ImageViewController作成（テスト用）

---
+++ PageViewControllerSample/ImageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ImageViewController.swift
+//  PageViewControllerSample
+//
+//  Created by shima jinsei on 2017/01/16.
+//  Copyright © 2017年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var pageIndex: Int = 0
+    var data: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+
+    }
+
+    func setData(pageIndex: Int, data: String) {
+        self.pageIndex = pageIndex
+        self.data = data
+        
+        let label = UILabel()
+        label.frame = CGRect(x:0,y:0,width:200,height:40)
+        label.text  = "page\(pageIndex)-" + data
+        label.center = view.center
+        view.addSubview(label)
+    }
+
+}
